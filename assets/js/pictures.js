21ba77f38bcc2310b8a0c41035f2d99fa0084d42
mako7200 <k168sa@gmail.com>
2024-02-06 03:30:56 +0900

    微調整

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -2,7 +2,7 @@
 //テキストのカウントアップ+バーの設定
 var bar = new ProgressBar.Line(splash_text, {//id名を指定
 	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-	duration: 1000,//時間指定(1000＝1秒)
+	duration: 2000,//時間指定(1000＝1秒)
 	strokeWidth: 0.2,//進捗ゲージの太さ
 	color: 'yellow',//進捗ゲージのカラー
 	trailWidth: 0.1,//ゲージベースの線の太さ
